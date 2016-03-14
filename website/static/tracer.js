1ac225bcf2fb73e0539de1500075f55b37ac1c41
Peter Ashwell <s.trefely@gmail.com>
2016-03-13 20:09:58 -0400

    Whatever

--- website/static/tracer.js
+++ website/static/tracer.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
     console.log('wassup lol');
+
+    function doTracerAnalysis(labeled, unlabeled) {
+        formData = $('#tracerForm').serialize()
+        $.post('/api/tracer', formData).then(function(a, b, c) {
+            console.log('did request:', a, b, c);
+        });
+    }
+ 
+    function onClickComputePercentages() {
+
+    } 
+    
+    $('#computePercentages').click(doTracerAnalysis);
+
 });
