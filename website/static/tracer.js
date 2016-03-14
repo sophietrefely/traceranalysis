a47577327c5b2034f8061f702ada5b12fb7532e4
Peter Ashwell <s.trefely@gmail.com>
2016-03-13 20:54:30 -0400

    Add error message

--- website/static/tracer.js
+++ website/static/tracer.js
@@ -3,13 +3,15 @@ $(document).ready(function() {
 
     function doTracerAnalysis(labeled, unlabeled) {
         formData = $('#tracerForm').serialize()
-        $.post('/api/tracer', formData).then(function(data) {
+        $.post('/api/tracer', formData).done(function(data) {
             var arrays = JSON.parse(data);
             var lines = []
             arrays.forEach(function(row) {
                 lines.push(row.join('\t'));
             });
             $('#results').val(lines.join('\n'));
+        }).fail(function() {
+            alert('Something went wrong! Contact s.trefely@gmail.com with the data you tried to use.');
         });
     }
     
