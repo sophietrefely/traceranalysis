f5ba0767679e765148cbc9e52f4bcf623b2b09b6
sophietrefely <s.trefely@gmail.com>
2016-03-19 15:08:13 -0400

    Compute data size upon paste, handle MS \r issue

--- website/static/tracer.js
+++ website/static/tracer.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
         alert('Something went wrong! Contact s.trefely@gmail.com with the data you tried to use.');
     }
 
+    function knownBadData(where) {
+        message = 'Your data seems to be malformed at row: ' + where.row + ' column: ', where.column + '. ' +
+                  'Please check for trailing tabs or lines, and that each row and column is consistent';
+        alert(message);
+    }
+
     function doTracerAnalysis(labeled, unlabeled) {
         formData = $('#tracerForm').serialize()
         $.post('/api/tracer', formData).done(function(data) {
@@ -21,6 +27,66 @@ $(document).ready(function() {
             showAlert();
         });
     }
-    
+
+    function findRowsAndColumns(data) {
+        console.log('raw data:', data);
+        // First, remove any trailling or preceding newlines
+        var data = data.trim();
+
+        // Replace \r with \n in case that is an issue
+        // Fuck you microsoft
+        data = data.replace(/\r\n/g, '\n');
+        data = data.replace(/\r/g, '\n');
+
+        // Split by newline to count number of rows
+        var rows = data.split('\n');
+
+        var numberOfColumns = null;
+        rows.forEach(function(row, index) {
+            // Trim any trailing tabs or other whitespace
+            row = row.trim();
+
+            // Split row to get the length and to check column consistency
+            var rowSplit = row.split('\t');
+            var length = rowSplit.length;
+
+            // If numberOfColumns is defined, we have already seen a row length
+            // Check it is the same as the one before with compound if statement
+            if (numberOfColumns && numberOfColumns !== length) {
+                var where = {
+                    row: index + 1,
+                    column: length + 1
+                };
+                knownBadData(where);
+            }
+
+            // Assign to numberOfColumns so we can check consistency as we go
+            numberOfColumns = length;
+        })
+
+        return {
+            rows: rows.length,
+            columns: numberOfColumns
+        };
+    }
+
+    function getMetadataForUnlabeled(e) {
+        var input = e.originalEvent.clipboardData.getData('Text');
+        console.log('compute metadata for:', input);
+        console.log('size of data:', findRowsAndColumns(input));
+
+    }
+
+    function getMetadataForLabeled(e) {
+        var input = e.originalEvent.clipboardData.getData('Text');
+        console.log('compute metadata for:', input);
+        console.log('size of data:', findRowsAndColumns(input));
+    }
+
+
+
     $('#computePercentages').click(doTracerAnalysis);
+
+    $('#unlabeledData').on('paste', getMetadataForUnlabeled);
+    $('#labeledData').on('paste', getMetadataForLabeled)
 });
